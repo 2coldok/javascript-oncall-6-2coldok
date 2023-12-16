6cfa7d95f7be8830bb0bf0037b62ba82f941d2df
coldok <2coldok@gmail.com>
2023-12-16 14:52:07 +0900

    feat : 해당 달의 모든 평일 주말 휴일 배열 생성 기능 구현

--- src/domain/Calendar.js
+++ src/domain/Calendar.js
@@ -32,9 +32,24 @@ class Calendar {
   }
 
   bringWeek() {
-    const WEEK = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
+    const WEEK = ['일', '월', '화', '수', '목', '금', '토'];
     
     return WEEK[this.#date.getDay()];
+  }
+  
+
+  bringWeekdayOrHoliday() {
+    if (this.bringMonth() === 1 && this.bringDate() === 1) return '휴일';
+    if (this.bringMonth() === 3 && this.bringDate() === 1) return '휴일';
+    if (this.bringMonth() === 5 && this.bringDate() === 5) return '휴일';
+    if (this.bringMonth() === 6 && this.bringDate() === 6) return '휴일';
+    if (this.bringMonth() === 8 && this.bringDate() === 15) return '휴일';
+    if (this.bringMonth() === 10 && this.bringDate() === 3) return '휴일';
+    if (this.bringMonth() === 10 && this.bringDate() === 9) return '휴일';
+    if (this.bringMonth() === 12 && this.bringDate() === 25) return '휴일';
+    if (this.bringWeek() === '토' || this.bringWeek() === '일') return '주말';
+
+    return '평일';
   }
   
   // 해당 달의 마지막 일수를 구해준다(28 or 29 or 30 or 31)
--- src/domain/Match.js
+++ src/domain/Match.js
@@ -1,3 +1,4 @@
+import Calendar from "./Calendar.js";
 /*
 - 기본적으로 순번에 따라 비상 근무일을 배정한다.
 - 비상 근무자는 평일 순번, 휴일 순번에 각각 1회 편성되어야 한다.(ex.평일 순번에 같은 인원이 두번 배정 불가능)
@@ -7,10 +8,14 @@
 다음 평일 근무자와 순서를 바꿔서 근무한다.
 - 비상 근무자 배정 시 다음 근무자와 순서를 바꿔야 하는 경우에는, 앞의 날짜부터 순서를 변경해야 한다.(최근 꺼부터 차례대로 변경 ㅇㅇ)
 */
+//연속 2일 근무 하는지 체크
+//순서 바꾸는 기능 구 현
 
 class Match {
   weekWorkers;
   holidayWorkers;
+
+  calendarArray = [];
 
   
   constructor() {
@@ -18,8 +23,23 @@ class Match {
   }
   
   // 원본. ex ('5','월')
+  // calendarArray = ['평일', '평일', '평일', '평일', '휴일', '주말', '주말', '평일',.....] 로 세팅해준다.
   setMonthWithWeek(monthStr, weekStr) {
-
+    const month = Number(monthStr);
+    const lastDate = Calendar.getLastDayOfMonth(20203, month);
+    const startDate = [];
+    for (let i = 1; i <= lastDate; i++) {
+      const find = new Calendar(2023, month, i);
+      if (find.bringWeek() === weekStr) {
+        startDate.push(find.bringDate());
+        break;
+      }
+    }
+    const [start] = startDate;
+    for (let i = start; i <= lastDate; i++) {
+      const calendar = new Calendar(2023, month, i);
+      this.calendarArray.push(calendar.bringWeekdayOrHoliday());
+    }
   }
   
   // 원본
@@ -37,3 +57,10 @@ class Match {
 }
 
 export default Match;
+/*
+const a = new Match();
+
+
+
+*/
+
