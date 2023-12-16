7769c2b845cc5f158f8edc735397c09d5718f7aa
coldok <2coldok@gmail.com>
2023-12-16 15:35:38 +0900

    feat : 일반적으로 순서대로 비상 인원 배치 기능 구현

--- src/domain/Match.js
+++ src/domain/Match.js
@@ -1,6 +1,6 @@
 import Calendar from "./Calendar.js";
 /*
-- 기본적으로 순번에 따라 비상 근무일을 배정한다.
+- 기본적으로 순번에 따라 비상 근무일을 배정한다.(1)
 - 비상 근무자는 평일 순번, 휴일 순번에 각각 1회 편성되어야 한다.(ex.평일 순번에 같은 인원이 두번 배정 불가능)
 - 비상근무자 연속 2일 근무 불가능 
 - 순번상 특정 근무자가 연속 2일 근무하게 되는 상황에는, 다음 근무자와 순서를 바꿔 편성한다.
@@ -14,10 +14,12 @@ import Calendar from "./Calendar.js";
 class Match {
   weekWorkers;
   holidayWorkers;
+  calendarArray = []; // ['평일', '평일', '평일', '평일', '휴일', '주말', '주말', '평일',.....]
 
-  calendarArray = [];
+  matchArray = [];
 
-  
+  startDate;
+  lastDate;  
   constructor() {
 
   }
@@ -26,17 +28,15 @@ class Match {
   // calendarArray = ['평일', '평일', '평일', '평일', '휴일', '주말', '주말', '평일',.....] 로 세팅해준다.
   setMonthWithWeek(monthStr, weekStr) {
     const month = Number(monthStr);
-    const lastDate = Calendar.getLastDayOfMonth(20203, month);
-    const startDate = [];
-    for (let i = 1; i <= lastDate; i++) {
+    this.lastDate = Calendar.getLastDayOfMonth(20203, month);
+    for (let i = 1; i <= this.lastDate; i++) {
       const find = new Calendar(2023, month, i);
       if (find.bringWeek() === weekStr) {
-        startDate.push(find.bringDate());
+        this.startDate = find.bringDate();
         break;
       }
     }
-    const [start] = startDate;
-    for (let i = start; i <= lastDate; i++) {
+    for (let i = this.startDate; i <= this.lastDate; i++) {
       const calendar = new Calendar(2023, month, i);
       this.calendarArray.push(calendar.bringWeekdayOrHoliday());
     }
@@ -52,6 +52,36 @@ class Match {
     this.holidayWorkers = str.split(',');
   }
 
+  weekIndex = 0;
+  holidayIndex = 0;
+  // input : w(평일) or h(휴일)
+  updateIndex(str) {
+    const maxIndex = this.holidayWorkers.length - 1;
+    if (str === 'w' && this.weekIndex === maxIndex) return this.weekIndex = 0;
+    if (str === 'h' && this.holidayIndex === maxIndex) return this.holidayIndex = 0;
+    
+    if (str === 'w' && this.weekIndex !== maxIndex) {
+      return this.weekIndex += 1;
+    }
+    if (str === 'h' && this.holidayIndex !== maxIndex) {
+      return this.holidayIndex += 1;
+    }
+    
+  }
+  
+  match() {
+    this.calendarArray.forEach((status) => {
+      if (status === '평일') {
+        this.matchArray.push(this.weekWorkers[this.weekIndex]);
+        this.updateIndex('w');
+      } else {
+        this.matchArray.push(this.holidayWorkers[this.holidayIndex]);
+        this.updateIndex('h');
+      } 
+    });
+  }
+
+
 
 
 }
@@ -59,8 +89,13 @@ class Match {
 export default Match;
 /*
 const a = new Match();
+a.setMonthWithWeek('5', '월');
+a.setWeekWorkers('평1,평2,평3,평4,평5');
+a.setHolidayWorkers('홀1,홀2,홀3,홀4,홀5');
+a.match();
 
-
-
+console.log(a.matchArray);
 */
+
+
 
